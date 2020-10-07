35c9c330b774df9a61c82ce5ce326b3a6c745ab3
donnyramadhan-dr <ramadhandonny60@gmail.com>
2020-10-07 16:04:43 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi RestApi ku berjalan", res)
 };
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, ) {
+        if (error) {
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
